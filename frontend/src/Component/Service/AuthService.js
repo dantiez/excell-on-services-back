fe625631d0d9a17de78a8d4c593a8a393b538d01
KhusDna <stelladevolic304@gmail.com>
2024-12-30 20:35:20 -0500

    add

---
+++ frontend/src/Component/Service/AuthService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import config from "../config";
+
+const API_BASE_URL = `${config.baseUrl}/Auth`;
+
+export const AuthService = {
+  login: async (userData) => {
+    try {
+      const { userData } = await axios.post(`${API_BASE_URL}/login`, {
+        email: data.email,
+        password: data.password,
+      });
+      return userData;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  logout: async (accessToken) => {
+    try {
+      await axios.post(
+        `${API_BASE_URL}/logout`,
+        {},
+        {
+          headers: {
+            Authorization: Bearer`${accessToken}`,
+          },
+        },
+      );
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+
+  refresh_token: async (userId, refresh_token) => {
+    try {
+      const token = await axios.get(`${API_BASE_URL}/refresh_token`, {
+        userId,
+        refresh_token,
+      });
+      return token;
+    } catch (error) {}
+  },
+  signup: async (signupData) => {
+    try {
+      const { userEmail } = await axios.post(`${API_BASE_URL}/signup`, {
+        ...signupData,
+      });
+      return userEmail;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+};
